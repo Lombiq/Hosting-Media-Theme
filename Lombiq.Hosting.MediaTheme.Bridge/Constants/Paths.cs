a7ba014f972fe4dfb640f854e9f21f2fd9183f57
Márk Bartha <barthamark@gmail.com>
2022-07-31 20:55:19 +0200

    Change default folder names

--- Lombiq.Hosting.MediaTheme.Bridge/Constants/Paths.cs
+++ Lombiq.Hosting.MediaTheme.Bridge/Constants/Paths.cs
@@ -3,7 +3,8 @@
 public static class Paths
 {
     public const string MediaThemeRootFolder = "_MediaTheme";
-    public const string MediaThemeTemplatesFolder = "Views";
-    public const string MediaThemeAssetsFolder = "wwwroot";
+    public const string MediaThemeTemplatesFolder = "Templates";
+    public const string MediaThemeAssetsFolder = "Assets";
     public const string MediaThemeAssetsWebPath = MediaThemeRootFolder + "/" + MediaThemeAssetsFolder;
+    public const string MediaThemeTemplatesWebPath = MediaThemeRootFolder + "/" + MediaThemeTemplatesFolder;
 }
