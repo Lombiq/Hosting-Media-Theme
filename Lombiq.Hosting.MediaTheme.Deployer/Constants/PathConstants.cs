eb74d3681ea893293bd6039e523659ab8fadc1ff
Gábor Pór <gabor.por@lombiq.com>
2022-11-21 16:19:35 +0100

    Fixing typo

--- Lombiq.Hosting.MediaTheme.Deployer/Constants/PathConstants.cs
+++ Lombiq.Hosting.MediaTheme.Deployer/Constants/PathConstants.cs
@@ -6,10 +6,10 @@ public static class PathConstants
     public const string LocalThemeWwwRootDirectory = "wwwroot";
     public const string LocalThemeViewsDirectory = "Views";
     public const string MediaThemeTemplatesDirectory = "Templates";
-    public const string MediaThemeAssetsDriectory = "Assets";
+    public const string MediaThemeAssetsDirectory = "Assets";
 
-    public const string MediaThemeAssetsWebPath = MediaThemeRootDirectory + "/" + MediaThemeAssetsDriectory;
-    public const string MediaThemeAssetsCopyDirectoryPath = "\\" + MediaThemeRootDirectory + "\\" + MediaThemeAssetsDriectory;
+    public const string MediaThemeAssetsWebPath = MediaThemeRootDirectory + "/" + MediaThemeAssetsDirectory;
+    public const string MediaThemeAssetsCopyDirectoryPath = "\\" + MediaThemeRootDirectory + "\\" + MediaThemeAssetsDirectory;
 
     public const string MediaThemeTemplatesWebPath = MediaThemeRootDirectory + "/" + MediaThemeTemplatesDirectory;
     public const string MediaThemeTemplatesCopyDirectoryPath = "\\" + MediaThemeRootDirectory + "\\" + MediaThemeTemplatesDirectory;
