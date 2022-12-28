745509afbe3b16d1aed235e7c7db7c97f3a3f9f6
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-12-28 03:12:51 +0100

    Fixing Linux incompatibility

--- Lombiq.Hosting.MediaTheme.Deployer/Constants/PathConstants.cs
+++ Lombiq.Hosting.MediaTheme.Deployer/Constants/PathConstants.cs
@@ -1,4 +1,5 @@
 namespace Lombiq.Hosting.MediaTheme.Deployer.Constants;
+
 public static class PathConstants
 {
     public const string MediaThemeRootDirectory = "_MediaTheme";
@@ -8,12 +9,15 @@ public static class PathConstants
     public const string MediaThemeTemplatesDirectory = "Templates";
     public const string MediaThemeAssetsDirectory = "Assets";
 
+    // This needs to use slashes on every platform.
     public const string MediaThemeAssetsWebPath = MediaThemeRootDirectory + "/" + MediaThemeAssetsDirectory;
-    public const string MediaThemeAssetsCopyDirectoryPath = "\\" + MediaThemeRootDirectory + "\\" + MediaThemeAssetsDirectory;
 
     public const string MediaThemeTemplatesWebPath = MediaThemeRootDirectory + "/" + MediaThemeTemplatesDirectory;
     public const string MediaThemeTemplatesCopyDirectoryPath = "\\" + MediaThemeRootDirectory + "\\" + MediaThemeTemplatesDirectory;
 
-    public const string RecipeFile = "\\Recipe.json";
+    public const string RecipeFile = "Recipe.json";
     public const string LiquidFileExtension = ".liquid";
+
+    public static readonly string MediaThemeAssetsCopyDirectoryPath =
+        Path.DirectorySeparatorChar + MediaThemeRootDirectory + Path.DirectorySeparatorChar + MediaThemeAssetsDirectory;
 }
