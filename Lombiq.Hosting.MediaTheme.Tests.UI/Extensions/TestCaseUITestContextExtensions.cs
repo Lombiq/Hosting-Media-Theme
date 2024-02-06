95a409eae8cbdfa7004fb45af5cd660c6e833efe
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-02-07 00:14:55 +0100

    Adding test

--- Lombiq.Hosting.MediaTheme.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.Hosting.MediaTheme.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -8,15 +8,25 @@ namespace Lombiq.Hosting.MediaTheme.Tests.UI.Extensions;
 
 public static class TestCaseUITestContextExtensions
 {
-    public static async Task TestMediaThemeTemplateRenderingBehaviorAsync(this UITestContext context)
+    public static async Task TestMediaThemeDeployedBehaviorAsync(this UITestContext context)
     {
-        await context.SignInDirectlyAsync();
         await context.ExecuteMediaThemeSampleRecipeDirectlyAsync();
         await context.GoToMediaThemeTestContentPageAsync();
+        AssertElements(context, "v");
+    }
 
-        context.Exists(By.XPath("//head//link[contains(@href, '/mediatheme/example.css?v=')]").Hidden());
+    public static async Task TestMediaThemeLocalBehaviorAsync(this UITestContext context)
+    {
+        await context.SetThemeDirectlyAsync("Lombiq.Hosting.MediaTheme.Tests.Theme");
+        await context.GoToHomePageAsync(onlyIfNotAlreadyThere: false);
+        AssertElements(context, "mediatheme");
+    }
+
+    private static void AssertElements(UITestContext context, string cacheBustingParameterName)
+    {
+        context.Exists(By.XPath($"//head//link[contains(@href, '/mediatheme/example.css?{cacheBustingParameterName}=')]").Hidden());
         context.Exists(By.XPath("//p[contains(., 'This is an example template hosted in Media Theme.')]"));
-        context.Exists(By.XPath("//main//img[contains(@src, '/mediatheme/example.png')]"));
-        context.Exists(By.XPath("//main//img[contains(@src, '/mediatheme/example2.png?v=')]"));
+        context.Exists(By.XPath("//img[contains(@src, '/mediatheme/example.png')]"));
+        context.Exists(By.XPath($"//img[contains(@src, '/mediatheme/example2.png?{cacheBustingParameterName}=')]"));
     }
 }
