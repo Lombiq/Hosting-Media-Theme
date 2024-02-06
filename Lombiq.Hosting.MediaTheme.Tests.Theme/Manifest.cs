94cb90e3b39bf9530b7f52ad4aa6ff0fbcd3df42
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-02-06 23:50:43 +0100

    Adding test Media Theme theme

---
+++ Lombiq.Hosting.MediaTheme.Tests.Theme/Manifest.cs
@@ -0,0 +1,11 @@
+using OrchardCore.DisplayManagement.Manifest;
+using static Lombiq.Hosting.MediaTheme.Bridge.Constants.FeatureNames;
+
+[assembly: Theme(
+    Name = "Lombiq Hosting - Media Theme - Test Theme",
+    Author = "Lombiq Technologies",
+    Website = "https://github.com/Lombiq/Hosting-Media-Theme",
+    Version = "0.0.1",
+    Description = "A theme only used for testing that demonstrates the local development version of a Media Theme.",
+    Dependencies = new[] { MediaThemeBridge }
+)]
---
+++ Lombiq.Hosting.MediaTheme.Tests.Theme/Readme.md
@@ -0,0 +1,10 @@
+# Lombiq Hosting - Media Theme - Test Theme
+
+A theme only used for testing that demonstrates the local development version of a Media Theme.
+
+Media Themes are used in two distinct modes:
+
+- As a standard theme during local development.
+- From the Media folder when deployed.
+
+We need to test both cases. This project tests the standard theme scenario while the sample in `Lombiq.Hosting.MediaTheme` provides the content for testing the deployed scenario.
