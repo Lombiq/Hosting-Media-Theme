5a27d52c8c89048618872d6ec5641f41a18bd8fc
Sára El-Saig <sara.el-saig@lombiq.com>
2026-03-29 01:30:17 +0100

    Fix new analyzer warnings.

--- Lombiq.Hosting.MediaTheme.Deployer/Program.cs
+++ Lombiq.Hosting.MediaTheme.Deployer/Program.cs
@@ -169,7 +169,7 @@ internal static partial class Program
         {
             var manifestPath = Path.Combine(themePath, "Manifest.cs");
             var manifestContent = await File.ReadAllTextAsync(manifestPath);
-            var baseThemeMatch = BaseThemeRegex().Match(manifestContent);
+            var baseThemeMatch = BaseThemeRegex.Match(manifestContent);
 
             if (baseThemeMatch.Success)
             {
@@ -346,5 +346,5 @@ internal static partial class Program
     }
 
     [GeneratedRegex(@"BaseTheme\s*=\s*""(?<baseThemeId>.*)""", RegexOptions.ExplicitCapture, matchTimeoutMilliseconds: 1000)]
-    private static partial Regex BaseThemeRegex();
+    private static partial Regex BaseThemeRegex { get; }
 }
